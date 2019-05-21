d056fc42be96a66fa6a85eb77d504a6f62c69ef1
Laura Holstege <laholstege@ucdavis.edu>
2019-05-21 13:33:41 -0700

    add more card info

--- src/Labs.Mvc/Models/CardModel.cs
+++ src/Labs.Mvc/Models/CardModel.cs
@@ -1,12 +1,33 @@
+using System;
+
 namespace Labs.Mvc.Models
 {
     public class CardModel
     {
         public string CardsId { get; set; }
+        
         public string nCardholderId { get; set; }
+        
         public string strEmployeeId { get; set; }
+        
         public string strFirstName { get; set; }
+        
         public string strLastName { get; set; }
+        
+        public string Department { get; set; }
+        
         public string strEncodedCardNumber { get; set; }
+        
+        public DateTime dtExpirationDate { get; set; }
+        
+        public bool nActive { get; set; }
+        
+        public string Access1 { get; set; }
+        
+        public string Access2 { get; set; }
+        
+        public string nFacilityCode { get; set; }
+
+        public string strCardFormatName { get; set; }
     }
 }
